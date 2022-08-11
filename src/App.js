ee0cc6464163ea06a8293e18b11d1a774211363a
Gosha Gaponov <egoriks1994@gmail.com>
2022-08-11 23:49:16 +0300

    11 blocks 011 Feat: create Context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header/Header.js";
 import Meals from "./components/Meals/Meals.js";
 import Cart from "./components/Cart/Cart.js";
 import { useState } from "react";
+import CartProvider from "./store/CartProvider.js";
 
 const App = () => {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -13,13 +14,13 @@ const App = () => {
     setCartIsShown(false);
   };
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 };
 export default App;
