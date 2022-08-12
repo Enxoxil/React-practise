67a3184b8705be75427c494a41e35385412b7f83
Gosha Gaponov <egoriks1994@gmail.com>
2022-08-12 08:12:06 +0300

    11 blocks 013 Feat: create state & reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context.js";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatetItems = state.items.concat(action.item);
+    const updatetTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatetItems,
+      totalAmount: updatetTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", payload: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", payload: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
