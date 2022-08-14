d19f0a9ea5ca50e20884c75e9b6c1ac58c74d86d
Gosha Gaponov <egoriks1994@gmail.com>
2022-08-15 00:26:56 +0300

    11 blocks 015 Feat: outputting cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal/Modal.js";
 import classes from "./Cart.module.css";
+import CartContext from "./../../store/cart-context.js";
+import CartItem from "./CartItem.js";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {
+    console.log(id);
+  };
+  const cartItemAddHandler = (item) => {
+    console.log(item);
+  };
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          // onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onRemove={cartItemRemoveHandler.bind(item.id)}
+          // onAdd={cartItemAddHandler.bind(null, item)}
+          onAdd={cartItemAddHandler}
+        />
       ))}
     </ul>
   );
@@ -15,13 +36,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total amount</span>
-        <span>33.33</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onHideCart}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}{" "}
       </div>
     </Modal>
   );
